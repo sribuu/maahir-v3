e35e527a08f290c67c113a9baf37193e8d396337
milyasbpa <basilyas.2015@gmail.com>
2022-11-15 18:56:08 +0700

    fix: config

--- stg_deploy.sh
+++ stg_deploy.sh
@@ -5,7 +5,7 @@ scp -i ~/.ssh/ilyaskprsribuu.pem -r ./src ubuntu@11.0.33.51:/home/ubuntu/next_ma
 # assets
 scp -i ~/.ssh/ilyaskprsribuu.pem -r ./public ubuntu@11.0.33.51:/home/ubuntu/next_maahir
 # environment
-scp -i ~/.ssh/ilyaskprsribuu.pem -r ./.env.localhost ubuntu@11.0.33.51:/home/ubuntu/next_maahir
+scp -i ~/.ssh/ilyaskprsribuu.pem -r ./.env.staging ubuntu@11.0.33.51:/home/ubuntu/next_maahir
 # config
 scp -i ~/.ssh/ilyaskprsribuu.pem -r ./next-env.d.ts ubuntu@11.0.33.51:/home/ubuntu/next_maahir
 scp -i ~/.ssh/ilyaskprsribuu.pem -r ./next.config.js ubuntu@11.0.33.51:/home/ubuntu/next_maahir
