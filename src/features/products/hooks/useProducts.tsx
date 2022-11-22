89a5b743a31d499fa3ea0179860ae4ff533d81d0
milyasbpa <basilyas.2015@gmail.com>
2022-11-22 11:32:43 +0700

    feat: filter in products

---
+++ src/features/products/hooks/useProducts.tsx
@@ -0,0 +1,49 @@
+import { fetchInfinityListProducts } from "@/src/core/lib/api/dynamic";
+import { ReactQueryKey } from "@/src/core/lib/constants";
+import { IProducts } from "@/src/core/lib/models";
+import { useInfiniteQuery, QueryFunctionContext } from "@tanstack/react-query";
+import { useState } from "react";
+
+export interface IProductsQueryParams {
+  page_param: number;
+  category_id?: number;
+  max_price?: number;
+  min_price?: number;
+}
+
+export const useProductsQuery = (params: IProductsQueryParams) =>
+  useInfiniteQuery<IProducts[], IProductsQueryParams>({
+    queryKey: [ReactQueryKey.GetInfinityProductList, params],
+    queryFn: ({
+      queryKey,
+    }: QueryFunctionContext<[string, IProductsQueryParams]>) => {
+      const [_, data] = queryKey;
+      console.log(data, "ini data query harusnya");
+      return fetchInfinityListProducts(params);
+    },
+    
+    getNextPageParam: (_: IProducts[], pages: IProducts[][]) => {
+      console.log(pages.length, "ini apa");
+      return pages.length + 1;
+    },
+  });
+
+export const useProductsState = (params: IProductsQueryParams) => {
+  const [state, setState] = useState<IProductsQueryParams>({
+    page_param: 1,
+  });
+
+  const handleChangeProductCategory = (
+    e?: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const result =
+      state.category_id === parseInt(e.currentTarget.id)
+        ? -1
+        : parseInt(e.currentTarget.id);
+    setState({ ...state, category_id: result });
+  };
+  return {
+    state,
+    handleChangeProductCategory,
+  };
+};
